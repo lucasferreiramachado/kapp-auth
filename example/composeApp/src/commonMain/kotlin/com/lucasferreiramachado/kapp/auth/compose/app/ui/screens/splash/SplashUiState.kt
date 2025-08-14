2bdb72826c3bd8a6e4baa4a1037b88131e9d7e4f
Lucas Ferreira Machado <lucasferreiramachadopro@gmail.com>
2025-08-14 13:57:49 -0300

    Merge remote-tracking branch 'origin/main'

--- example/composeApp/src/commonMain/kotlin/com/lucasferreiramachado/kapp/auth/compose/app/ui/screens/splash/SplashUiState.kt
+++ example/composeApp/src/commonMain/kotlin/com/lucasferreiramachado/kapp/auth/compose/app/ui/screens/splash/SplashUiState.kt
@@ -1,6 +1,6 @@
 package com.lucasferreiramachado.kapp.auth.compose.app.ui.screens.splash
 
 data class SplashUiState(
-    val title: String = "KAppProduct",
+    val title: String = "KAppAuth",
     val subtitle: String = "Example Compose App"
 )
